ca0eb1455e8a9c09003d44d9ccc333b048410e8b
andydeliot <A.andy@hotmail.fr>
2017-10-01 03:11:08 +0200

    Ajout d'un test, simulation de combat dans combazzz

--- combazzz.py
+++ combazzz.py
@@ -2,45 +2,217 @@
 
 temps_recolte = 30 * 60
 
-class fourmi:
-    def __init__(self, nom, vie, attaque, defense, temps, nourriture):
+
+class Fourmi:
+    def __init__(self, nom, vie, attaque, defense, temps, nourriture, nombre=1):
         self.nom = nom
 
         self.vie = vie
         self.attaque = attaque
         self.defense = defense
-        
         self.temps = temps
         self.nourriture = nourriture
 
-        self.afficher_stat()
+        # Stats.
+        self.nombre_par_recolte = temps_recolte / self.temps
+        self.vie_par_recolte = self.vie * self.nombre_par_recolte
+        self.attaque_par_recolte = self.attaque * self.nombre_par_recolte
+        self.defense_par_recolte = self.defense * self.nombre_par_recolte
+        self.nourriture_par_recolte = self.nourriture * self.nombre_par_recolte
 
-    def afficher_stat(self):
-        print(self.nom)
-        nombre_par_recolte = temps_recolte / self.temps
-        print("Nombre pour une récolte :", nombre_par_recolte)
-        vie_par_recolte = self.vie * nombre_par_recolte
-        print("Vie totale :", vie_par_recolte)
-        attaque_par_recolte = self.attaque * nombre_par_recolte
-        print("Attaque totale :", attaque_par_recolte)
-        defense_par_recolte = self.defense * nombre_par_recolte
-        print("Defense totale :", defense_par_recolte)
-        nourriture_par_recolte = self.nourriture * nombre_par_recolte
-        print("Nourriture totale :", nourriture_par_recolte)
-
-        print("-"*50)
+    def __repr__(self):
+        return self.nom
 
 
+class Troupe:
+    def __init__(self, fourmi, nombre):
+        self.fourmi = fourmi
+        self.nombre = nombre # N'interviens pas dans les stats.
 
-jsn = fourmi("Jeune soldate naine", 8, 3, 2, 22, 16)
-sn = fourmi("Soldate naine", 10, 5, 4, 32, 20)
-ne = fourmi("Naine d'élite", 13, 7, 6, 41, 26)
-js = fourmi("Jeune soldate", 16, 10, 9, 53, 30)
-s = fourmi("Soldate", 20, 15, 14, 72, 36)
-c = fourmi("Concierge", 30, 1, 25, 101, 70)
-a = fourmi("Artilleuse",10, 30, 15, 103, 30)
-ae = fourmi("Artilleuse d'élite", 12, 35, 18, 109, 34)
-se = fourmi("Soldate d'élite", 72, 24, 23, 104, 44)
-t = fourmi("Tank", 35, 55, 1, 134, 100)
-tu = fourmi("Tueuse", 50, 50, 50, 197, 80)
-tue = fourmi("Tueuse d'élite", 55, 55, 55, 197, 90)
+        self.vie = self.fourmi.vie * self.nombre
+        self.attaque = self.fourmi.attaque * self.nombre
+        self.defense = self.fourmi.defense * self.nombre
+        self.temps = self.fourmi.temps * self.nombre
+        self.nourriture = self.fourmi.nourriture * self.nombre
+
+    def egale(self, other):
+        assert type(other) is Troupe, "Le comparateur n'est pas de type Troupe."
+        return True if self.fourmi is other.fourmi and self.nombre == other.nombre else False
+
+
+    def __repr__(self):
+        return self.fourmi.nom + "(" + str(self.nombre) + ")"
+
+
+class Armee:
+    def __init__(self, *troupes):
+        self.troupes = [Troupe(troupe.fourmi, troupe.nombre) for troupe in troupes if troupe.nombre > 0]
+
+        self.vie = 0
+        self.attaque = 0
+        self.defense = 0
+        self.temps = 0
+        self.nourriture = 0
+        for troupe in self.troupes:
+            self.vie += troupe.vie
+            self.attaque += troupe.attaque
+            self.defense += troupe.defense
+            self.temps += troupe.temps
+            self.nourriture += troupe.nourriture
+
+    def egale(self, other):
+        assert type(other) is Armee, "Le comparateur n'est pas de type Armee."
+        if len(self.troupes) != len(other.troupes):
+            return False
+        i = 0
+        while True:
+            if i >= len(self.troupes):
+                break
+            if not self.troupes[i].egale(other.troupes[i]):
+                return False
+            i += 1
+        return True
+
+    def __eq__(self, other):
+        assert type(other) is Armee, "Impossible de comparé autre chose qu'une armée."
+        attaquante1, defenseuse1 = self.combattre(other)
+        attaquante2, defenseuse2 = other.combattre(self)
+        return attaquante1.vivante() and attaquante2.vivante()
+
+    def vivante(self):
+        """ Retourne vrai si l'armée n'est pas vide de troupes. """
+        return self.troupes != []
+
+    def combattre(self, armee):
+        """ Fait combatre les deux armées. Retourne également les deux armées, l'armée attaquante en premiere position. """
+        armee_attaque = self
+        armee_defense = armee
+        while armee_attaque.vivante() and armee_defense.vivante():
+            if armee_attaque.attaque >= armee_defense.vie * 3:
+                coef_defense = 0.1
+            elif armee_attaque.attaque >= armee_defense.vie * 2:
+                coef_defense = 0.3
+            elif armee_attaque.attaque >= armee_defense.vie * 1.5:
+                coef_defense = 0.5
+            else:
+                coef_defense = 1
+            armee_defense = armee_attaque.attaquer(armee_defense, armee_attaque.attaque)
+            armee_attaque = armee_defense.attaquer(armee_attaque, armee_defense.defense*coef_defense)
+            print("")
+        return armee_attaque, armee_defense
+
+    def attaquer(self, armee, degat):
+        """ Retourne l'armée attaqué. """
+        print(self)
+        print("Les attaquantes infligent {0} points de dégats.".format(degat))
+
+        troupes = []
+        for troupe in armee.troupes:
+            tue = int(degat / troupe.fourmi.vie)
+            tue = troupe.nombre if tue > troupe.nombre else tue
+
+            degat -= tue * troupe.fourmi.vie
+            troupes.append(Troupe(troupe.fourmi, troupe.nombre - tue))
+            print("Les défenseuses perdent {0} {1}.".format(tue, troupe.fourmi.nom))
+
+        return Armee(*troupes)
+
+    def difference(self, armee):
+        """ Retourne une armée représentant la différence entre les deux armées. Les deux armées doivent avoir les même types de troupes. """
+        for troupe in armee.troupes:
+            assert troupe.fourmi in [troupe.fourmi for troupe in self.troupes], "La troupe de l'armée n'existe pas"
+
+        troupes = []
+        for troupe in armee.troupes:
+            for troupe2 in self.troupes:
+                if troupe.fourmi is troupe2.fourmi:
+                    troupes.append(Troupe(troupe.fourmi, troupe2.nombre - troupe.nombre))
+
+        return Armee(*troupes)
+
+    def __repr__(self):
+        return str(self.troupes)
+
+
+
+jsn = Fourmi("Jeune soldate naine", 8, 3, 2, 22, 16)
+sn = Fourmi("Soldate naine", 10, 5, 4, 32, 20)
+ne = Fourmi("Naine d'élite", 13, 7, 6, 41, 26)
+js = Fourmi("Jeune soldate", 16, 10, 9, 53, 30)
+s = Fourmi("Soldate", 20, 15, 14, 72, 36)
+c = Fourmi("Concierge", 30, 1, 25, 101, 70)
+a = Fourmi("Artilleuse",10, 30, 15, 103, 30)
+ae = Fourmi("Artilleuse d'élite", 12, 35, 18, 109, 34)
+se = Fourmi("Soldate d'élite", 27, 24, 23, 104, 44)
+t = Fourmi("Tank", 35, 55, 1, 134, 100)
+tu = Fourmi("Tueuse", 50, 50, 50, 197, 80)
+tue = Fourmi("Tueuse d'élite", 55, 55, 55, 197, 90)
+
+
+def statistiques():
+    fourmis = [jsn, sn, ne, js, s, c, a, ae, se, t, tu, tue]
+
+    fourmis_nombre = sorted(fourmis, key=lambda f: f.nombre_par_recolte, reverse=True)
+    print("Nombre :", fourmis_nombre)
+
+    fourmis_vie = sorted(fourmis, key=lambda f: f.vie_par_recolte, reverse=True)
+    print("Vie :", fourmis_vie)
+
+    fourmis_attaque = sorted(fourmis, key=lambda f: f.attaque_par_recolte, reverse=True)
+    print("Attaque :", fourmis_attaque)
+
+    fourmis_defense = sorted(fourmis, key=lambda f: f.defense_par_recolte, reverse=True)
+    print("Défense :", fourmis_defense)
+
+    fourmis_nourriture = sorted(fourmis, key=lambda f: f.nourriture_par_recolte, reverse=True)
+    print("Nourriture :", fourmis_nourriture)
+
+
+
+if __name__ == "__main__":
+    armee1 = Armee(Troupe(jsn, 1000), Troupe(a, 500))
+    armee2 = Armee(Troupe(c, 300), Troupe(s, 500))
+
+    armee3 = armee1.combattre(armee2)
+
+    mon_armee = Armee(Troupe(jsn, 2493), Troupe(se, 502), Troupe(ne, 338), Troupe(js, 2000), Troupe(a, 382))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_fourmizzz.py
@@ -0,0 +1,123 @@
+import os
+import pytest
+from combazzz import *
+
+# Setup.
+
+@pytest.fixture()
+def troupe1():
+    return Troupe(jsn, 1000)
+
+
+def test_troupe():
+    troupe = Troupe(jsn, 1000)
+    assert troupe.vie == 8000
+    assert troupe.attaque == 3000
+    assert troupe.defense == 2000
+    assert troupe.nourriture == 16000
+    assert troupe.__repr__() == "Jeune soldate naine(1000)"
+
+    assert troupe.egale(Troupe(jsn, 1000))
+    assert not troupe.egale(Troupe(jsn, 999))
+    assert not troupe.egale(Troupe(s, 1000))
+
+def test_armee():
+    armee = Armee(Troupe(jsn, 1000), Troupe(s, 500))
+    assert armee.vie == 18000
+    assert armee.attaque == 10500
+    assert armee.defense == 9000
+    assert armee.nourriture == 34000
+
+    assert armee.egale(Armee(Troupe(jsn, 1000), Troupe(s, 500)))
+    assert not armee.egale(Armee(Troupe(jsn, 999), Troupe(s, 500)))
+    assert not armee.egale(Armee(Troupe(jsn, 1000), Troupe(se, 500)))
+
+
+def test_attaquer():
+    armee1 = Armee(Troupe(jsn, 1000))
+    armee2 = Armee().attaquer(armee1, 4000)
+    assert armee2.vie == 4000
+    assert armee2.troupes[0].nombre == 500
+
+def test_combattre():
+    armee1 = Armee(Troupe(jsn, 1000))
+    armee2 = Armee(Troupe(jsn, 1000))
+    armee3, armee4 = armee1.combattre(armee2)
+    assert armee3.__repr__() == "[Jeune soldate naine(762)]"
+
+def test_difference():
+    armee1 = Armee(Troupe(jsn, 1000), Troupe(s, 500))
+    armee2 = Armee(Troupe(jsn, 800), Troupe(s, 400))
+    armee3 = armee1.difference(armee2)
+    assert armee3.egale(Armee(Troupe(jsn, 200), Troupe(s, 100)))
+    assert not armee3.egale(Armee(Troupe(jsn, 100), Troupe(s, 100)))
+    assert not armee3.egale(Armee(Troupe(jsn, 200), Troupe(se, 100)))
+
+def test_operateur():
+    armee1 = Armee(Troupe(jsn, 1000))
+    armee2 = Armee(Troupe(jsn, 1000))
+    assert armee1 == armee2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    msg = os.popen("py.test")# -vv")
+    print(msg.read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
