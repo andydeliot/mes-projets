252a3a7517441dcb6093933514de2a1303cc1b52
Andydeliot <A.andy@hotmail.fr>
2017-10-18 14:07:41 +0200

    Youtubing

---
+++ youtubing.py
@@ -0,0 +1,15 @@
+
+import youtube_dl
+
+url = str(input("Url de la vidéo : "))
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
